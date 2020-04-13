b2f5092d9f9cd267ce4f81b50b35c9f3b66b18e7
JasonMacKeigan <2447285+JasonMacKeigan@users.noreply.github.com>
2020-04-13 12:38:51 -0300

    world_teleport: converted CooldownDurationFieldListener to use LongConfigFieldListener to reduce lines.

--- world_teleport/src/main/java/com/jsonmack/worldteleport/config/field/CooldownDurationFieldListener.java
+++ world_teleport/src/main/java/com/jsonmack/worldteleport/config/field/CooldownDurationFieldListener.java
@@ -3,6 +3,7 @@ package com.jsonmack.worldteleport.config.field;
 import com.jsonmack.mcplugins.config.field.ConfigFieldListener;
 import com.jsonmack.mcplugins.config.field.ConfigFieldAcceptanceResult;
 import com.jsonmack.mcplugins.config.field.ConfigFieldListenerParseException;
+import com.jsonmack.mcplugins.config.field.impl.LongConfigFieldListener;
 import com.jsonmack.worldteleport.config.TeleportModuleConfig;
 
 import static com.jsonmack.mcplugins.config.field.ConfigFieldAcceptanceResult.ok;
@@ -11,20 +12,11 @@ import static com.jsonmack.mcplugins.config.field.ConfigFieldAcceptanceResult.un
 /**
  * Created by Jason MK on 2020-04-11 at 6:28 p.m.
  */
-public class CooldownDurationFieldListener implements ConfigFieldListener<TeleportModuleConfig, Long> {
+public class CooldownDurationFieldListener implements LongConfigFieldListener<TeleportModuleConfig> {
 
     @Override
     public ConfigFieldAcceptanceResult acceptable(Long value) {
         return value < 0 ? unacceptable("Duration must be non-negative.") : ok();
-    }
-
-    @Override
-    public Long parse(String argument) throws ConfigFieldListenerParseException {
-        try {
-            return Long.parseLong(argument);
-        } catch (NumberFormatException nfe) {
-            throw new ConfigFieldListenerParseException(String.format("Unable to parse %s, expected Long.", argument));
-        }
     }
 
     @Override
