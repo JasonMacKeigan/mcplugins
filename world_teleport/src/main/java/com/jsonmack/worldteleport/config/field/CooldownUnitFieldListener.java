20d8d5d1af665a3a900d213f907d260e79ae3578
JasonMacKeigan <2447285+JasonMacKeigan@users.noreply.github.com>
2020-04-13 12:38:16 -0300

    world_teleport: converted some field listeners classes to use api implementations to reduce lines.

--- world_teleport/src/main/java/com/jsonmack/worldteleport/config/field/CooldownUnitFieldListener.java
+++ world_teleport/src/main/java/com/jsonmack/worldteleport/config/field/CooldownUnitFieldListener.java
@@ -3,6 +3,7 @@ package com.jsonmack.worldteleport.config.field;
 import com.jsonmack.mcplugins.config.field.ConfigFieldListener;
 import com.jsonmack.mcplugins.config.field.ConfigFieldAcceptanceResult;
 import com.jsonmack.mcplugins.config.field.ConfigFieldListenerParseException;
+import com.jsonmack.mcplugins.config.field.impl.TimeUnitConfigFieldListener;
 import com.jsonmack.worldteleport.config.TeleportModuleConfig;
 
 import java.util.concurrent.TimeUnit;
@@ -12,20 +13,11 @@ import static com.jsonmack.mcplugins.config.field.ConfigFieldAcceptanceResult.ok
 /**
  * Created by Jason MK on 2020-04-11 at 6:28 p.m.
  */
-public class CooldownUnitFieldListener implements ConfigFieldListener<TeleportModuleConfig, TimeUnit> {
+public class CooldownUnitFieldListener implements TimeUnitConfigFieldListener<TeleportModuleConfig> {
 
     @Override
     public ConfigFieldAcceptanceResult acceptable(TimeUnit value) {
         return ok();
-    }
-
-    @Override
-    public TimeUnit parse(String argument) throws ConfigFieldListenerParseException {
-        try {
-            return TimeUnit.valueOf(argument.toUpperCase());
-        } catch (IllegalStateException nfe) {
-            throw new ConfigFieldListenerParseException(String.format("Unable to parse %s, expected TimeUnit.", argument));
-        }
     }
 
     @Override
--- world_teleport/src/main/java/com/jsonmack/worldteleport/config/field/TilesPerDiamondFieldListener.java
+++ world_teleport/src/main/java/com/jsonmack/worldteleport/config/field/TilesPerDiamondFieldListener.java
@@ -3,6 +3,7 @@ package com.jsonmack.worldteleport.config.field;
 import com.jsonmack.mcplugins.config.field.ConfigFieldListener;
 import com.jsonmack.mcplugins.config.field.ConfigFieldAcceptanceResult;
 import com.jsonmack.mcplugins.config.field.ConfigFieldListenerParseException;
+import com.jsonmack.mcplugins.config.field.impl.IntegerConfigFieldListener;
 import com.jsonmack.worldteleport.config.TeleportModuleConfig;
 
 import static com.jsonmack.mcplugins.config.field.ConfigFieldAcceptanceResult.*;
@@ -10,20 +11,11 @@ import static com.jsonmack.mcplugins.config.field.ConfigFieldAcceptanceResult.*;
 /**
  * Created by Jason MK on 2020-04-10 at 10:15 p.m.
  */
-public class TilesPerDiamondFieldListener implements ConfigFieldListener<TeleportModuleConfig, Integer> {
+public class TilesPerDiamondFieldListener implements IntegerConfigFieldListener<TeleportModuleConfig> {
 
     @Override
     public ConfigFieldAcceptanceResult acceptable(Integer value) {
         return value < 0 ? unacceptable("The value must be non-negative.") : ok();
-    }
-
-    @Override
-    public Integer parse(String argument) throws ConfigFieldListenerParseException {
-        try {
-            return Integer.parseInt(argument);
-        } catch (NumberFormatException nfe) {
-            throw new ConfigFieldListenerParseException();
-        }
     }
 
     @Override
